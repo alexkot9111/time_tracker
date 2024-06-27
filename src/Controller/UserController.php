698acdec04d174cbdaf9efdd0eac2f8f78c6b55a
Alex K <kotalex911@gmail.com>
2024-06-27 17:46:27 +0300

    Validation messages

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\UserType;
 use App\Repository\UserRepository;
 use App\Service\CurrentCompanyService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -41,23 +40,36 @@ class UserController extends Controller
         $currentCompany = $this->currentCompanyService->getCurrentCompany();
         $data = json_decode($request->getContent(), true);
 
+        if(empty($data['email'])) {
+            return new JsonResponse(['errors' => ['email' => 'Email can not be empty']], 400);
+        }
+
         // Check if email already exist
         $existingUser = $this->userRepository->findOneByEmail($data['email']);
         if ($existingUser) {
-            return new JsonResponse('Email already exists', 400);
+            return new JsonResponse(['errors' => ['email' => 'Email already exists']], 400);
         }
 
+        // Set properties of the User entity
         $user = new User();
         $user->setCompanyId($currentCompany);
         $user->setEmail($data['email']);
-        $user->setFirstName($data['first_name']);
-        $user->setLastName($data['last_name']);
+        $user->setFirstName($data['first_name'] ?? '');
+        $user->setLastName($data['last_name'] ?? '');
         $user->setCreated(new \DateTime());
 
+        // Validate the User entity
         $errors = $validator->validate($user);
 
         if (count($errors) > 0) {
-            return new JsonResponse((string) $errors, 400);
+            // Transform Symfony's ConstraintViolationListInterface into an array of error messages
+            $errorMessages = [];
+            foreach ($errors as $error) {
+                $errorMessages[$error->getPropertyPath()] = $error->getMessage();
+            }
+
+            // Return JSON response with error messages and status code 400 (Bad Request)
+            return new JsonResponse(['errors' => $errorMessages], 400);
         }
 
         $entityManager->persist($user);
